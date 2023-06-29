508d8126f99a976bb7865903eda2d51fa38d4f3b
msnoodijk001 <ams.snoodijk@gmail.com>
2023-06-29 12:24:41 +0200

    opdrachten af

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -4,6 +4,8 @@
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 namen zou bevatten!
 // ==========================================
 
+console.log('Opdracht 1:');
+
 const names = ["Henk", "Piet", "Fred", "Joop"];
 
 // Verwachtte uitkomsten:
@@ -12,9 +14,9 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
-
-
-
+for ( let i= 0; i < names.length; i++ ) {
+    console.log(names[i] + 'je');
+}
 
 // ==========================================
 // Opdracht 2
@@ -29,10 +31,18 @@ const numbers = [2, 4, 5, 29, 38];
 // console.log(numbers) geeft: [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 4, 8, 15, 87, 76 ];
+console.log('Opdracht 2:');
 
+console.log(numbers);
 
-
-
+for (let i = 0; i < numbers.length; i++) {
+if (numbers[i] % 2 === 0) {
+    numbers[i] = numbers[i] * 2;
+} else {
+    numbers[i] = numbers[i] * 3;
+}
+}
+console.log(numbers);
 
 // ==========================================
 // Opdracht 3
@@ -56,6 +66,13 @@ const squares = [30, 2, 8, 24, 11];
 //   'Het volume van 24 is 13824',
 //   'Het volume van 11 is 1331'
 // ]
+
+console.log('Opdracht 3:');
+
+for (let i = 0; i < squares.length; i++) {
+    const volume = squares[i] * squares[i] * squares[i];
+    console.log(`Het volume van ${squares[i]} is ${volume}`);
+}
 
 
 
